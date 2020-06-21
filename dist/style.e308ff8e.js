3b3089f1fea589a1e96b05b0535ee5e696f5cc1a
olanetsoft <heedris2olubisi@gmail.com>
2020-06-21 12:45:36 +0100

    styles updated

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images\\bg2.jpeg":[["bg2.e8f75571.jpeg","images/bg2.jpeg"],"images/bg2.jpeg"],"./images\\undraw_online_payments_luau.png":[["undraw_online_payments_luau.82eb0041.png","images/undraw_online_payments_luau.png"],"images/undraw_online_payments_luau.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./images\\bg2.jpeg":[["bg2.e8f75571.jpeg","images/bg2.jpeg"],"images/bg2.jpeg"],"./images\\down-arrow.svg":[["down-arrow.5a527256.svg","images/down-arrow.svg"],"images/down-arrow.svg"],"./images\\undraw_online_payments_luau.png":[["undraw_online_payments_luau.82eb0041.png","images/undraw_online_payments_luau.png"],"images/undraw_online_payments_luau.png"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52826" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58621" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- js/alert.js
+++ js/alert.js
@@ -11,6 +11,6 @@ export const showAlert = (type, msg) =>{
     hideAlert()
     const markup = `<div class="alert alert--${type}">${msg}</div>`;
     document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-    window.setTimeout(hideAlert, 7000)
+    window.setTimeout(hideAlert, 5000)
 
 };
